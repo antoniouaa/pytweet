0b17920b1ffccd326e1cfd9986467907fa8fc819
antoniouaa <antoniouaa@hotmail.com>
2020-03-05 18:18:14 +0000

    uncommented twython constructor as it was slowing down the script and wasn't used

--- pytweet/twitter_bot.py
+++ pytweet/twitter_bot.py
@@ -13,23 +13,27 @@ def batch_query(query):
 
     df = pd.DataFrame(post_)
     df.sort_values(by="favorite_count", inplace=True, ascending=False)
-    df.head(5)
+    #df.to_csv("saved_tweets.csv", sep=",")
+    return df
 
-credentials = {}
-credentials["CONSUMER_KEY"] = "ELtHFSlF6oyC62WoHvmi9pBTK" 
-credentials["CONSUMER_SECRET"] = "gcfn0y6PVXBLAHbqCZhQFUBsteCWgPiF2BQnkbGPvUNoTQE3oF"
-credentials["ACCESS_TOKEN"] = "780845339796393984-FT9bNGCOwBlMYSuHy120zBF2g7yYcY0" 
-credentials["ACCESS_SECRET"] = "LxX9ARDyFkWlwoGTZXAVjSdB90zB7J0mwT19guJSnUO6l"  
+credentials = {
+        "CONSUMER_KEY" : "ELtHFSlF6oyC62WoHvmi9pBTK",
+        "CONSUMER_SECRET" : "gcfn0y6PVXBLAHbqCZhQFUBsteCWgPiF2BQnkbGPvUNoTQE3oF",
+        "ACCESS_TOKEN" : "780845339796393984-FT9bNGCOwBlMYSuHy120zBF2g7yYcY0",
+        "ACCESS_SECRET" : "LxX9ARDyFkWlwoGTZXAVjSdB90zB7J0mwT19guJSnUO6l"}
 
 sample_query = {"q": "trump",
                 "result_type": "popular",
-                "count": 10,
+                "count": 1000,
                 "lang": "en"}
 
-search_term = input("Search for: ")
 
 #python_tweets = Twython(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
+#df = batch_query(sample_query)
+#print(df.head(10))
+#res = python_tweets.cursor(python_tweets.search, q="trump")
 
+search_term = input("Search for: ")
 stream = StreamingBot(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"], credentials["ACCESS_TOKEN"], credentials["ACCESS_SECRET"])
 stream.statuses.filter(track=search_term)
 
